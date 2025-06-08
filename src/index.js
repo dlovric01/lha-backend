e0399f40d4aba71870c5fcb3312873a4096f186e
dlovric-margins <danijel.lovric@valeremargins.com>
2025-06-08 12:22:35 +0200

    updated from localhost to all ips

--- src/index.js
+++ src/index.js
@@ -14,13 +14,13 @@ app.get('/', (_req, res) => {
 app.get('/trigger', async (_req, res) => {
   try {
     // Turn relay ON
-    await axios.get('http://192.168.1.26/relay/0?turn=on');
+    await axios.get('http://raspberrypi.local/relay/0?turn=on');
 
     // Wait 500 ms
     await new Promise(resolve => setTimeout(resolve, 500));
 
     // Turn relay OFF
-    await axios.get('http://192.168.1.26/relay/0?turn=off');
+    await axios.get('http://raspberrypi.local/relay/0?turn=off');
 
     res.send('Relay triggered (ON → OFF)');
   } catch (error) {
@@ -31,6 +31,7 @@ app.get('/trigger', async (_req, res) => {
 
 const server = http.createServer(app);
 
-server.listen(port, () => {
+server.listen(port, '0.0.0.0', () => {
   console.log(`Server running on port ${port}`);
 });
+
